aa622b46fb81d6b9b9fb099c452baa434bdc92ab
Derleyzim <filho.wanderley@hotmail.com>
2021-03-26 14:38:49 -0300

    Mudando algumas Pastas, adicionando o comando rm, e atualizando os nomes para formato correto

--- script.sh
+++ script.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-token=$(head -n 1 "/home/derleyzim/Área de Trabalho/TentandoAutomatizar/RelatoriosPermissoes/TokenDoGItHub")
+token=$(head -n 1 "/home/derleyzim/Desktop/TentandoAutomatizar/Permissoes/TokenDoGItHub")
 
 while read linguagem dono repositorio; do
-  lin_don_rep="${linguagem}_${dono}_${repositorio}"
+  lin_rep="${linguagem}_${repositorio}"
 
       > requesicao2.txt
 
@@ -18,6 +18,8 @@ while read linguagem dono repositorio; do
 
       grep -A 6 number requesicao2.txt | grep -A 5 state | grep -A4 locked | grep -A 3 title | grep login | sed 's/"login": "//g' |sed 's/",//g' |sort | uniq -c | sort -n -r | cut -c 5-10 > apenasnumeros.txt
 
-      paste -d "," apenasnomes.txt apenasnumeros.txt > "/home/derleyzim/Área de Trabalho/TentandoAutomatizar/ArquivosFinais/$lin_don_rep.csv"
+      paste -d "," apenasnomes.txt apenasnumeros.txt > "/home/derleyzim/DesktopDadosAPI-do-GITHUB/ArquivosFinais/$lin_rep.csv"
 
-done < "/home/derleyzim/Área de Trabalho/TentandoAutomatizar/RelatoriosPermissoes/repositorios"
+done < "/home/derleyzim/Desktop/DadosAPI-do-GITHUB/Permissoes/repositorios"
+
+rm -f *.txt
