d0b044540f2689b68f3da3cb77b239e5d1ab52f1
Derleyzim <filho.wanderley@hotmail.com>
2021-03-26 13:47:59 -0300

    Atualização com automatizacao do script, e relatorio final

--- script.sh
+++ script.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 
-> requesicao2.txt
+token=$(head -n 1 "/home/derleyzim/Área de Trabalho/TentandoAutomatizar/RelatoriosPermissoes/TokenDoGItHub")
 
-for i in 1 2; do
-	curl -H "Accept: application/vnd.github.v3+json" "https://api.github.com/repos/freebsd/freebsd-src/pulls?state=closed&page=$i&per_page=100" >> requesicao2.txt
+while read linguagem dono repositorio; do
+  lin_don_rep="${linguagem}_${dono}_${repositorio}"
 
-done
+      > requesicao2.txt
 
-grep -A 3 title requesicao2.txt | grep login | sed 's/"login": "//g' |sed 's/",//g' |sort | uniq -c | sort -n -r | cut -c 15-100 > apenasnomes.txt
+    for i in 1 2; do
 
-grep -A 3 title requesicao2.txt | grep login | sed 's/"login": "//g' |sed 's/",//g' |sort | uniq -c | sort -n -r | cut -c 5-10 > apenasnumeros.txt
+     echo $i
+     echo $lin_don_rep
+      curl -u "derleymad:$token" -H  "Accept: application/vnd.github.v3+json" "https://api.github.com/repos/$dono/$repositorio/pulls?state=closed&page=$i&per_page=100" >> requesicao2.txt
+    done
 
-paste -d "," apenasnomes.txt apenasnumeros.txt > final.txt
+      grep -A 6 number requesicao2.txt | grep -A 5 state | grep -A 4 locked| grep -A 3 title | grep login | sed 's/"login": "//g' |sed 's/",//g' |sort | uniq -c | sort -n -r | cut -c 15-100 > apenasnomes.txt
 
-#| sort | uniq -c |sort -nk1,1
- 
-      
+      grep -A 6 number requesicao2.txt | grep -A 5 state | grep -A4 locked | grep -A 3 title | grep login | sed 's/"login": "//g' |sed 's/",//g' |sort | uniq -c | sort -n -r | cut -c 5-10 > apenasnumeros.txt
 
+      paste -d "," apenasnomes.txt apenasnumeros.txt > "/home/derleyzim/Área de Trabalho/TentandoAutomatizar/ArquivosFinais/$lin_don_rep.csv"
+
+done < "/home/derleyzim/Área de Trabalho/TentandoAutomatizar/RelatoriosPermissoes/repositorios"
